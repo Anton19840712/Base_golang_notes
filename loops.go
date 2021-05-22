8baa3161114a04830cf829b1056c9a3c9073a7df
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-22 17:25:09 +0300

    Add samples, how to assign variables in go.

---
+++ loops.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+//there is only one syntax for all kinds of cycles in go: onle for
+
+func main(){
+//------------------------------------------
+	for i := 0; i < 8; i++ {
+		fmt.Println("Antonio, save your soul", i)
+	}
+//------------------------------------------
+	for {
+		fmt.Println("Antonio, save your soul")
+	}//ctrl+c to exit
+}
+
+
+
