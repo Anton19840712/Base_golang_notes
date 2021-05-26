c148d76951d25df72add02b60f2c8e0f3546fbfd
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-26 03:12:34 +0300

    Change some comments.

--- loops.go
+++ loops.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//there is only one syntax for all kinds of cycles in go: onle for
+//there is only one syntax for for all kinds of cycles in go: only for
 func main(){
 //------------------------------------------
 	for i := 0; i < 8; i++ {
