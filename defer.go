4332dd6677bc7170886bb43b2e8a3f7e56f10a89
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-06-18 20:44:37 +0300

    Add some comments.

--- defer.go
+++ defer.go
@@ -6,8 +6,7 @@ import "fmt"
 func main() { //need the same as name of the package
 	for i:=0;i<3;i++{
 		fmt.Println(i)
-		defer fmt.Println(i, "defer") //means pass it to the end of execution plan and: 
-		// at first print "Hello, world!", and then only for cycle and from the end of stack!!!
+		defer fmt.Println(i, "defer") //means pass it to the end of the execution plan and:
 		fmt.Println(i)
 	}
 	fmt.Println("Hello, world!")
